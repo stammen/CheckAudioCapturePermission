c6ac1c4f6be4cb4b2081cbee6df66238e5b7eae2
Dale Stammen <dalestam@microsoft.com>
2018-06-27 09:02:48 -0700

    start of audio capture permission code

--- CheckAudioCapturePermission/MainPage.xaml.cs
+++ CheckAudioCapturePermission/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,14 +20,46 @@ using Windows.UI.Xaml.Navigation;
 
 namespace CheckAudioCapturePermission
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DeviceAccessInformation m_deviceAccessInformation;
+        
         public MainPage()
         {
             this.InitializeComponent();
+            
+            m_deviceAccessInformation = DeviceAccessInformation.CreateFromDeviceClass(DeviceClass.AudioCapture);
+            // The OnAccessChanged event never gets called!
+            m_deviceAccessInformation.AccessChanged += new TypedEventHandler<DeviceAccessInformation, DeviceAccessChangedEventArgs>(OnAccessChanged);
+        }
+
+        private async void OnAccessChanged(DeviceAccessInformation sender, DeviceAccessChangedEventArgs e)
+        {
+            var status = e.Status;
+            await SetStatus(status == DeviceAccessStatus.Allowed);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // it appears that m_deviceAccessInformation goes stale. Have to recreate it every time! 
+            m_deviceAccessInformation = DeviceAccessInformation.CreateFromDeviceClass(DeviceClass.AudioCapture);
+            var status = m_deviceAccessInformation.CurrentStatus;
+            await SetStatus(status == DeviceAccessStatus.Allowed);
+        }
+
+        private async Task SetStatus(bool enabled)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                if (enabled)
+                {
+                    permissionText.Text = "Audio Capture permission is denied";
+                }
+                else
+                {
+                    permissionText.Text = "Audio Capture permission is granted";
+                }
+            });
         }
     }
 }
